9cce3f768502d54afda52834d70dd9ea8af9fb45
Koussay <koussaydhifi48@gmail.com>
2021-12-18 13:56:34 +0100

    First commit

---
+++ src/Components/todo/todo.js
@@ -0,0 +1,27 @@
+import {useState} from 'react';
+
+const Todo = () => {
+    //States
+    const [input,setInput] = useState("");
+
+    //Functions for handling
+    const Onwrite = (e) =>{
+        var text = e.target.value;
+        setInput(text);
+    }
+
+
+    return (
+        <>
+        <h1>ToDoList</h1>
+        <form>
+        <input type="text" value={input} onChange={Onwrite}/><button type="submit">Add</button>
+        </form>
+        </>
+    );
+
+
+}
+
+export default Todo;
+
