6f744070c92e1b6fa8e0fc68de7b187a395b189d
Koussay <koussaydhifi48@gmail.com>
2021-12-19 11:07:42 +0100

    Add styling to button and some backend features

--- api/api.py
+++ api/api.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask,jsonify,request
 
 api = Flask(__name__)
 
 
-
+@api.route('/add',methods=["POST","GET"])
+def add():
+    data = request.get_json(force=True)
+    print(data)
+    return {"msg":data}
 
 
 if (__name__ == "__main__"):
--- src/Components/todo/todo.js
+++ src/Components/todo/todo.js
@@ -4,18 +4,30 @@ import './todo.css';
 const Todo = () => {
     //States
     const [input,setInput] = useState("");
-
+    const [msg,setMsg] = useState("");
     //Functions for handling
     const Onwrite = (e) =>{
         var text = e.target.value;
         setInput(text);
     }
 
+    const onSubmit = (e) => {
+        e.preventDefault()
+        fetch('/add',{
+            method:"post",
+            headers: {'Content-Type':'application/json', 'Accept': 'application/json'},
+            body:JSON.stringify({
+                "task":input
+            })
+        }).then(data=>data.json).then(data=>data.msg).catch(err=>console.error(err))
+    
+    }
 
     return (
         <>
         <h1>ToDoList</h1>
-        <form>
+        <small><p>{msg}</p></small>
+        <form onSubmit={onSubmit}>
         <input type="text" value={input} onChange={Onwrite}/><button type="submit">Add</button>
         </form>
         </>
